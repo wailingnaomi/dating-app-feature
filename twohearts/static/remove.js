dba1d1379b65984c8fca79c94a72842e4abb2650
wailingnaomi <naomicheung@live.nl>
2020-06-17 16:11:44 +0200

    search and session works

--- twohearts/static/remove.js
+++ twohearts/static/remove.js
@@ -1,36 +1,43 @@
+// Get dislike button by ID
 var remove = document.getElementById('dislike')
 
+// Add a click event
 if(remove){
     remove.addEventListener('click', onremove)
 }
 
 function onremove(ev){
-    // zoek de event.target
+    // Save event.target in node
     var node = ev.target
     
-    //console.log "node" om de event.target te zien
+    // Console log node to see the event.target
     console.log(node)
-    //console.log "node.dataset.value" om de id te zien
+
+    // Console log node.dataset.value to see the ID
     console.log(node.dataset.value)
 
+    // .....
     fetch(`/home/${node.dataset.value}`, {method: 'delete'})
     .then(onresponse)
     .then(onload)
     .catch(error)
     
-    //console.log "clicked" wanneer er wordt geclicked
+    // Console log clicked when clicked
     console.log('clicked')
 
+    // When clicked .......
     function onresponse(res){
         // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
         return JSON.parse('false'),
         console.log('succes')
     }
 
+    // When clicked reload the page
     function onload(){
      return   window.location.href = '/home'
     }
 
+    // When clicked give error
     // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
     function error(){
        console.log('Error')
